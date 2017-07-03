3a14c919fcc32074c67bada502f403961d4d0da3
Nilay Khatore <nkhatore@Nilays-MacBook-Air.local>
2017-07-03 14:41:14 -0700

    first commit

---
+++ src/Grid.java
@@ -0,0 +1,22 @@
+/**
+ * Created by nkhatore on 6/24/17.
+ */
+public class Grid {
+
+    public int[][] grid = new int[9][9];
+    public static int x;
+    public static int y;
+
+    public Grid() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Let's solve a Sudoku puzzle. Please enter the numbers from left to right, top to bottom.");
+        System.out.println("If a space is blank, denote it with a 0.");
+        System.out.println("What is the first number (top left)?");
+        while (y <= 9) {
+
+        }
+    }
+}
