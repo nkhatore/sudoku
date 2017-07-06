77796df14b347eaf1b75c5bd3a314d41697f8d7a
Nilay Khatore <nkhatore@Nilays-MacBook-Air.local>
2017-07-06 14:15:49 -0700

    add row/col checking, Coords

--- src/Solver.java
+++ src/Solver.java
@@ -1,4 +1,4 @@
-import java.util.HashSet;
+import java.util.Stack;
 
 /**
  * Created by nkhatore on 7/3/17.
@@ -6,60 +6,74 @@ import java.util.HashSet;
 public class Solver {
 
     private Grid grid;
-    private int x;
-    private int y;
+    private Coord curr;
+    private Stack<Coord> filledIn;
+
+    private class Coord {
+        public int x;
+        public int y;
+        private Coord(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     public Solver(Grid given) {
         grid = given;
-        x = 0;
-        y = 0;
+        curr = new Coord(0,0);
+        filledIn = new Stack<>();
         for (int[] arr : grid.grid) {
             fillRow(arr);
-            y++;
+            curr.x = 0;
+            curr.y++;
         }
     }
 
     private void fillRow(int[] row) {
         for (int cell : row) {
-            x++;
             if (cell == 0) {
                 for (int i = 1; i <= 9; i++) {
-                    checkBox(i);
+                    if (!checkBox(i) || !checkRow(i) || !checkCol(i)) {
+                        grid.grid[curr.y][curr.x] = i;
+                        filledIn.push(new Coord(curr.x, curr.y));
+                        break;
+                    }
                 }
             }
+            curr.x++;
         }
     }
 
     private boolean checkBox(int num) {
-        if (x < 3 && y < 3) {
+        if (curr.x < 3 && curr.y < 3) {
             if (grid.AC13.contains(num)) {
                 return false;
             }
-        } else if (x > 2 && x < 6 && y < 3) {
+        } else if (curr.x > 2 && curr.x < 6 && curr.y < 3) {
             if (grid.AC46.contains(num)) {
                 return false;
             }
-        } else if (x > 5 && x < 9 && y < 3) {
+        } else if (curr.x > 5 && curr.x < 9 && curr.y < 3) {
             if (grid.AC79.contains(num)) {
                 return false;
             }
-        } else if (x < 3 && y > 2 && y < 6) {
+        } else if (curr.x < 3 && curr.y > 2 && curr.y < 6) {
             if (grid.DF13.contains(num)) {
                 return false;
             }
-        } else if (x > 2 && x < 6 && y > 2 && y < 6) {
+        } else if (curr.x > 2 && curr.x < 6 && curr.y > 2 && curr.y < 6) {
             if (grid.DF46.contains(num)) {
                 return false;
             }
-        } else if (x > 5 && x < 9 && y > 2 && y < 6) {
+        } else if (curr.x > 5 && curr.x < 9 && curr.y > 2 && curr.y < 6) {
             if (grid.DF79.contains(num)) {
                 return false;
             }
-        } else if (x < 3 && y > 5 && y < 9) {
+        } else if (curr.x < 3 && curr.y > 5 && curr.y < 9) {
             if (grid.GI13.contains(num)) {
                 return false;
             }
-        } else if (x > 2 && x < 6 && y > 5 && y < 9) {
+        } else if (curr.x > 2 && curr.x < 6 && curr.y > 5 && curr.y < 9) {
             if (grid.GI46.contains(num)) {
                 return false;
             }
@@ -71,4 +85,25 @@ public class Solver {
         return true;
     }
 
+    private boolean checkRow(int num) {
+        for (int cell : grid.grid[curr.y]) {
+            if (cell == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkCol(int num) {
+        for (int i = 0; i < 9; i++) {
+            if (grid.grid[i][curr.x] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
